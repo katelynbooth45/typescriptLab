0284918a348fcb816e69c36a31ce20fb55d8cb00
Katelyn Booth <katelynbooth@katelyns-mbp.lan>
2020-03-30 19:25:40 -0400

    more updates

--- project.ts
+++ project.ts
@@ -1,44 +1,57 @@
 interface Mountain {
     name: string;
     height: number;
-    const ​mountains​: Mountains[] = [];
+    const ​mountains​: Mountain[] = [
+        {name: "Kilimanjaro", height: 19341},
+        {name: "Everest", height: 29029},
+        {name: "Denali", height: 20310}
+    ];
 }
 
 findNameOfTallestMountain = (Mountain) => {
-    for (i = 0; i < Mountain.length; i++) {
-        return 
-    }
+    // for (i = 0; i < mountain.length; i++) {
+    //     return 
+    // }
+    return Math.max.
 }
 findNameOfTallestMountain(mountains);
 
-console.log(findNameOfTallestMountain);
+console.log(tallestMountain);
 
 interface Product {
     name: string;
     price: number;
-    const ​products: Product[] = [];
+    const ​products: Product[] = [
+        {name: "Shirt", price: 10},
+        {name: "Shoes", price: 29},
+        {name: "Jacket", price: 50}
+    ];
 }
 
 calcAverageProductPrice = (Product) => {
-    for (i = 0; i < Product.length; i++) {
-        return 
-    }
+    // for (i = 0; i < products.length; i++) {
+    //     return 
+    // }
 }
 
 calcAverageProductPrice(products);
-console.log(calcAverageProductPrice);
+console.log(averagePrice);
 
 interface InventoryItem {
     product: Product;
     quantity: number;
-    const ​inventory​: InventoryItem[] = [];
+    const ​inventory​: InventoryItem[] = [
+        {product: "motor", quantity: 10},
+        {product: "sensor", quantity: 4},
+        {product: "LED", quantity: 20}
+    ];
 }
 
 calcInventoryValue = (InventoryItem) => {
-    for (i = 0; i < InventoryItem.length; i++) {
-        return 
-    }
+    // for (i = 0; i < InventoryItem.length; i++) {
+    //     return 
+    // }
 }
 
 calcInventoryValue(products);
-console.log(calcInventoryValue);
+console.log(inventoryValue);
