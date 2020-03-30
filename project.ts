0c4d66629d6e823f0cb9e672a162f5dee17ec17f
Katelyn Booth <katelynbooth@katelyns-mbp.lan>
2020-03-30 18:00:20 -0400

    first commit

---
+++ project.ts
@@ -0,0 +1,44 @@
+interface Mountain {
+    name: string;
+    height: number;
+    Mountain = mountains[];
+}
+
+findNameOfTallestMountain = (Mountain) => {
+    for (i = 0; i < Mountain.length; i++) {
+        return 
+    }
+}
+findNameOfTallestMountain(mountains);
+
+console.log(findNameOfTallestMountain);
+
+interface Product {
+    name: string;
+    price: number;
+    Products = products[];
+}
+
+calcAverageProductPrice = (Product) => {
+    for (i = 0; i < Mountain.length; i++) {
+        return 
+    }
+}
+
+calcAverageProductPrice(products);
+console.log(calcAverageProductPrice);
+
+interface InventoryItem {
+    product = Product;
+    quantity = number;
+    inventory = InventoryItem[];
+}
+
+calcInventoryValue = (InventoryItem) => {
+    for (i = 0; i < Mountain.length; i++) {
+        return 
+    }
+}
+
+calcInventoryValue(products);
+console.log(calcInventoryValue);
